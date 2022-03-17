34365ff63f5f9898517da85fce76d5043e0a6717
ChanYouJohn <101781010+ChanYouJohn@users.noreply.github.com>
2022-03-17 15:39:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cvwa5ieqhwwj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanyoujohn-employee"
+customregion = "us-east-1"
